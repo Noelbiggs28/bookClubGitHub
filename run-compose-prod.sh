de7699e2288d8b8333c652dc510e3c7e04b0e6bf
Noelbiggs28 <noelbiggs28@gmail.com>
2023-11-25 11:18:22 -1000

    yo

--- run-compose-prod.sh
+++ run-compose-prod.sh
@@ -18,5 +18,5 @@ docker-compose -f docker-compose.prod.yml up -d
 
 # make sure the postgres container is ready, then run migrations
 sleep 10 
-docker exec book_club-api-1 python /src/manage.py makemigrations 
-docker exec book_club-api-1 python /src/manage.py migrate
+docker exec bookclubgithub-api-1 python /src/manage.py makemigrations 
+docker exec bookclubgithub-api-1 python /src/manage.py migrate
